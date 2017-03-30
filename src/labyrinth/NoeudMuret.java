8a180e1a4d7058692a6d6cdef275367f401fb5cf
Andre Lalonde <xkaidran@gmail.com>
2017-03-29 22:37:31 -0400

    Created NoeudMuret

--- src/labyrinth/NoeudMuret.java
+++ src/labyrinth/NoeudMuret.java
@@ -4,5 +4,13 @@ package labyrinth;
  * @author Abdelhakim Qbaich
  * @author André Lalonde
  */
-public class NoeudMuret {
-}
+
+public class NoeudMuret{
+    private Muret muret;
+    private NoeudMuret suivant;
+
+    protected NoeudMuret(Muret m, NoeudMuret nm){
+        this.muret = m;
+        this.suivant = nm;
+    }
+ }
