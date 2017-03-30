27fa32f68159d5530b32887392779cd67503c594
Andre Lalonde <xkaidran@gmail.com>
2017-03-29 22:43:40 -0400

    ListeMuret

--- src/labyrinth/ListeMuret.java
+++ src/labyrinth/ListeMuret.java
@@ -4,5 +4,47 @@ package labyrinth;
  * @author Abdelhakim Qbaich
  * @author André Lalonde
  */
-public class ListeMuret {
-}
+
+ public class ListeMuret{
+
+    public NoeudMuret premierMuret;
+
+    //methodes de traitages
+
+    protected ListeMuret(){
+        this.premierMuret = null;
+    }
+
+    protected ListeMuret(NoeudMuret m){
+        muret = new NoeudMuret(m, premierMuret);
+        this.premierMuret = muret;
+    }
+
+    public void setVisible() {
+        tempNode = new NoeudMuret(premierMuret.muret, premierMuret);
+        while (tempNode != null) {
+            tempNode.muret.show();
+            tempNode = tempNode.suivant;
+        }
+    }
+
+    public void setInvisible() {
+        tempNode = new NoeudMuret(premierMuret.muret, premierMuret);
+        while (tempNode != null) {
+            tempNode.muret.hide();
+            tempNode = tempNode.suivant;
+        }
+    }
+
+    public Muret chercheMuret(Muret m) {
+        tempNode = new NoeudMuret(premierMuret.muret, premierMuret);
+        if (tempNode.muret.equals(m)) {
+            return tempNode.muret;
+        } else {
+            tempNode = tempNode.suivant;
+        }
+    }
+
+
+
+ }
