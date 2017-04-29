ee2aaa47ce7127381412c7d2db66450ce07ab9be
Abdelhakim Qbaich <abdelhakim@qbaich.com>
2017-04-29 12:02:13 -0400

    Usage of WASD and Arrows for movement

--- src/labyrinth/JPanelLaby.java
+++ src/labyrinth/JPanelLaby.java
@@ -22,15 +22,19 @@ public class JPanelLaby extends JPanel implements KeyListener {
     public void keyReleased(KeyEvent e) {
         switch (e.getKeyCode()) {
         case KeyEvent.VK_W:
+        case KeyEvent.VK_UP:
             Labyrinthe.deplace('H');
             break;
         case KeyEvent.VK_A:
+        case KeyEvent.VK_LEFT:
             Labyrinthe.deplace('G');
             break;
         case KeyEvent.VK_S:
+        case KeyEvent.VK_DOWN:
             Labyrinthe.deplace('B');
             break;
         case KeyEvent.VK_D:
+        case KeyEvent.VK_RIGHT:
             Labyrinthe.deplace('D');
             break;
         }
